83336cad6782a3e130609db999367c32cdbccce0
Akari <nakamura_akari@tre-inc.com>
2021-01-21 16:28:54 +0900

    ルーティング追加

---
+++ src/components/Buntton.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// export class Button extends React.Component {
+//   render() {
+//     return (
+//       <button>ボタン</button>
+//     )
+//   }
+// }
+
+function Button() {
+  return (
+    <button>ボタン</button>
+  )
+}
+
+export default Button
---
+++ src/components/Hello.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// export class Hello extends React.Component {
+//   render() {
+//     return (
+//       <div>Hello!</div>
+//     )
+//   }
+// }
+
+function Hello() {
+  return (
+    <div>Hello!</div>
+  )
+}
+
+export default Hello
